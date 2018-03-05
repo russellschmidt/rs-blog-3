709405603b945e6d3ccf5daff3d7e90b64fcd7fc
Russell Schmidt <russell.schmidt@me.com>
2018-03-04 23:30:21 -0800

    add image to About

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ import { css } from "glamor";
 import { rhythm } from "../utils/typography";
 
 let container = css({
-  height: `calc(100vh - 248px)`,
+  height: `calc(100vh - 100px)`,
 });
 
 let innerContainer = css({
@@ -19,7 +19,12 @@ let innerContainer = css({
 });
 
 let imageContainer = css({
-
+  gridColumn: `span 3`,
+  " img ": {
+    width: `100%`,
+    margin: 0,
+    padding: 0,
+  }
 });
 
 let sidebarContainer = css({
@@ -30,12 +35,21 @@ let articleContainer = css({
 
 });
 
+let portraitCarousel = [
+  `https://storage.googleapis.com/russellmschmidt-net-portfolio/portraits/russell-portrait-1.jpg`,
+  `https://storage.googleapis.com/russellmschmidt-net-portfolio/portraits/russell-portrait-2.jpg`,
+  `https://storage.googleapis.com/russellmschmidt-net-portfolio/portraits/russell-portrait-3.jpg`,
+  `https://storage.googleapis.com/russellmschmidt-net-portfolio/portraits/russell-portrait-4.jpg`,
+  `https://storage.googleapis.com/russellmschmidt-net-portfolio/portraits/russell-portrait-5.jpg`
+];
+
 export default () => (
   <div className={container}>
     <Helmet title={`About Russell Schmidt, a developer in Los Angeles, CA`}/>
     <h1>About</h1>
     <div className={innerContainer}>
       <div className={imageContainer}>
+        <img src={portraitCarousel[0]} alt="Pictures of me" />
       </div>
       <div className={sidebarContainer}>
       </div>
