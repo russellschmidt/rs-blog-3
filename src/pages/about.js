418f863aa5cc6ca3bfbb9cacc607ef653518c12b
Russell Schmidt <russell.schmidt@me.com>
2018-03-09 22:04:52 -0800

    more about me - education and languages

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,6 +8,25 @@ import { rhythm } from '../utils/typography';
 
 const container = css({
   height: '100%',
+  ' ul': {
+    listStyleType: 'none',
+    listStylePosition: 'outside',
+    margin: '0 0 1rem 0',
+  },
+  ' li, a': {
+    fontSize: 12,
+    margin: 0,
+    padding: 0
+  },
+  '@media (min-width: 768px)': {
+    ' h3': {
+      margin: '0 0 1rem 0',
+    },
+    ' h6': {
+      margin: '0 0 0.3rem 0',
+      fontStyle: 'normal',
+    }
+  }
 });
 
 const innerContainer = css({
@@ -24,15 +43,9 @@ const innerContainer = css({
 
 const imageContainer = css({
   gridColumn: 'span 12',
-  margin: '0 auto',
-  width: '100%',
-  ' img ': {
-    width: '100%',
-    margin: '0',
-    padding: 0,
-  },
   '@media (min-width: 768px)': {
     gridColumn: 'span 6',
+    width: '90%'
   },
 });
 
@@ -40,36 +53,13 @@ const articleContainer = css({
   gridColumn: 'span 8',
   '@media (min-width: 768px)': {
     gridColumn: 'span 4',
+    width: '92%',
   },
 });
 
 
 const sidenavContainer = css({
   gridColumn: 'span 4',
-  ' h6': {
-    fontStyle: 'normal',
-    margin: '0 0 0.4rem 0',
-    textTransform: 'uppercase',
-    fontSize: rhythm(0.5),
-    '@media (max-width: 767px)': {
-      fontSize: '0.5rem',
-    },
-  },
-  ' a': {
-    fontSize: rhythm(0.5),
-    color: '#555',
-    fontSize: rhythm(0.45),
-    '@media (max-width: 767px)': {
-      fontSize: '0.4rem',
-    },
-  },
-  ' ul': {
-    listStyleType: 'none',
-    margin: '0 0 0.8rem 0',
-  },
-  ' li': {
-    margin: 0,
-  },
   '@media (min-width: 768px)': {
     gridColumn: 'span 2',
   },
@@ -90,40 +80,109 @@ export default () => (
     <h1>About</h1>
     <div className={innerContainer}>
       <div className={imageContainer}>
-        <img src={portraitCarousel[1]} alt="Pictures of me" />
+        <img src={portraitCarousel[1]} alt="Pictures of Russell Schmidt" />
       </div>
       <div className={articleContainer}>
+        <article>
+          <h3>Biography</h3>
+          <p></p>
+        </article>
         <article>
           <h3>Education</h3>
           <ul>
             <li>
-              <h4>Master of Science in Integrated Design, Business and Technology</h4>
+              <h6>Master of Science in Integrated Design, Business and Technology</h6>
               <ul>
                 <li>University of Southern California (USC)</li>
                 <li>Iovine and Young Academy</li>
                 <li>2019 (candidate)</li>
                 <li><a href="https://design.usc.edu/about/" target="_blank">Design@USC</a></li>
-                <li />
               </ul>
             </li>
             <li>
-              <h4>Software Engineering Apprenticeship</h4>
+              <h6>Bachelor of Arts in Economics</h6>
+              <ul>
+                <li>Harvard University</li>
+                <li>2001</li>
+                <li><a href="https://economics.harvard.edu/" target="_blank">Economics</a></li>
+              </ul>
+            </li>
+            <li>
+              <h6>Software Engineering Apprenticeship</h6>
               <ul>
                 <li>Bloc</li>
-                <li>Iovine and Young Academy</li>
                 <li>2017</li>
-                <li><a href="https://design.usc.edu/about/" target="_blank">Design@USC</a></li>
+                <li><a href="https://blog.bloc.io/announcing-software-engineering-track/" target="_blank">Software Engineering Track</a></li>
+              </ul>
+            </li>
+            <li>
+              <h6>Certificate in Mobile Development (iOS/Swift)</h6>
+              <ul>
+              <li>General Assembly</li>
+                <li>2015</li>
+                <li><a href="https://generalassemb.ly/education/ios-development" target="_blank">iOS Development</a></li>
+              </ul>
+            </li>
+            <li>
+              <h6>Certificate in Entrepreneurship</h6>
+              <ul>
+                <li>Goldman Sachs' 10,000 Small Businesses</li>
+                <li>2014</li>
+                <li><a href="http://www.goldmansachs.com/citizenship/10000-small-businesses/US/" target="_blank">GS10KSB</a></li>
+              </ul>
+            </li>
+            <li>
+              <h6>Certificate in Digital Marketing</h6>
+              <ul>
+              <li>General Assembly</li>
+                <li>2013</li>
+                <li><a href="https://generalassemb.ly/education/digital-marketing" target="_blank">Digital Marketing &amp; Strategy</a></li>
+              </ul>
+            </li>
+            <li>
+              <h6>Back-End Web Development</h6>
+              <ul>
+                <li>General Assembly</li>
+                <li>2012</li>
+                <li><a href="https://generalassemb.ly/education/back-end-web-development" target="_blank">Back-End Web Development</a></li>
               </ul>
             </li>
           </ul>
         </article>
       </div>
       <div className={sidenavContainer}>
-        <h6>Links</h6>
+        <h3>Links</h3>
+        <h6>CV</h6>
         <ul>
           <li><a href="https://storage.googleapis.com/russellmschmidt-net-portfolio/resume/Schmidt_Russell_Resume.pdf" target="_blank">Resume</a></li>
         </ul>
-        <h6>Contributions</h6>
+        <h6>Languages & Frameworks</h6>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>SASS/SCSS</li>
+          <li>JavaScript</li>
+          <li>jQuery</li>
+          <li>React</li>
+          <li>React Native</li>
+          <li>Gatsby</li>
+          <li>Angular v1</li>
+          <li>Meteor</li>
+          <li>Ruby</li>
+          <li>Ruby on Rails</li>
+          <li>Jekyll</li>
+          <li>PHP</li>
+          <li>WordPress</li>
+        </ul>
+        <h6>Databases & Backend</h6>
+        <ul>
+          <li>SQL</li>
+          <li>PostgreSQL</li>
+          <li>MongoDB</li>
+          <li>GraphQL</li>
+          <li>Firebase</li>
+        </ul>
+        <h6>Open Source Contributions</h6>
         <ul>
           <li><a href="https://github.com/stympy/faker" target="_blank">Faker</a></li>
           <li><a href="https://github.com/botpress/botpress/pull/177" target="_blank">Botpress</a></li>
