52d9b7653bb57f527fbc1098f1676ecbb36ebe80
Russell Schmidt <russell.schmidt@me.com>
2018-03-09 20:14:24 -0800

    add eslint and some about business

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,61 +1,59 @@
-import React from "react";
-import Link from "gatsby-link";
-import Helmet from "react-helmet";
-import g from "glamorous";
-import { css } from "glamor";
+import React from 'react';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
+import g from 'glamorous';
+import { css } from 'glamor';
 
-import { rhythm } from "../utils/typography";
+import { rhythm } from '../utils/typography';
 
-let container = css({
+const container = css({
   // height: `calc(100vh - 270px)`,
 });
 
-let innerContainer = css({
-  display: `grid`,
-  gridTemplateColumns: `repeat(12, 8%)`,
-  gridTemplateRows: `auto`,
-  width: `100%`,
-  justifyContent: `space-evenly`,
+const innerContainer = css({
+  display: 'grid',
+  gridTemplateColumns: 'repeat(12, 8%)',
+  gridTemplateRows: 'auto',
+  width: '100%',
+  justifyContent: 'space-evenly',
 });
 
-let imageContainer = css({
-  gridColumn: `span 3`,
-  " img ": {
-    width: `100%`,
+const imageContainer = css({
+  gridColumn: 'span 3',
+  ' img ': {
+    width: '100%',
     margin: 0,
     padding: 0,
-  }
+  },
 });
 
-let sidebarContainer = css({
-
-});
-
-let articleContainer = css({
+const sidebarContainer = css({
 
 });
 
-let portraitCarousel = [
-  `https://storage.googleapis.com/russellmschmidt-net-portfolio/portraits/russell-portrait-1.jpg`,
-  `https://storage.googleapis.com/russellmschmidt-net-portfolio/portraits/russell-portrait-2.jpg`,
-  `https://storage.googleapis.com/russellmschmidt-net-portfolio/portraits/russell-portrait-3.jpg`,
-  `https://storage.googleapis.com/russellmschmidt-net-portfolio/portraits/russell-portrait-4.jpg`,
-  `https://storage.googleapis.com/russellmschmidt-net-portfolio/portraits/russell-portrait-5.jpg`
+const articleContainer = css({
+
+});
+
+const portraitCarousel = [
+  'https://storage.googleapis.com/russellmschmidt-net-portfolio/portraits/russell-portrait-1.jpg',
+  'https://storage.googleapis.com/russellmschmidt-net-portfolio/portraits/russell-portrait-2.jpg',
+  'https://storage.googleapis.com/russellmschmidt-net-portfolio/portraits/russell-portrait-3.jpg',
+  'https://storage.googleapis.com/russellmschmidt-net-portfolio/portraits/russell-portrait-4.jpg',
+  'https://storage.googleapis.com/russellmschmidt-net-portfolio/portraits/russell-portrait-5.jpg',
 ];
 
 
 export default () => (
   <div className={container}>
-    <Helmet title={`About Russell Schmidt, a developer in Los Angeles, CA`}/>
+    <Helmet title="About Russell Schmidt, a developer in Los Angeles, CA" />
     <h1>About</h1>
     <div className={innerContainer}>
       <div className={imageContainer}>
         <img src={portraitCarousel[0]} alt="Pictures of me" />
       </div>
-      <div className={sidebarContainer}>
-      </div>
-      <div className={articleContainer}>
-      </div>
+      <div className={sidebarContainer} />
+      <div className={articleContainer} />
     </div>
   </div>
 );
