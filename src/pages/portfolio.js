1a88ff96e5240d95ae71fbeda058901e40dc4df8
Russell Schmidt <russell.schmidt@me.com>
2018-03-31 13:41:20 -0700

    add cool rotating animation to portfolio

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -4,6 +4,20 @@ import Helmet from 'react-helmet';
 import g from 'glamorous';
 import { css } from 'glamor';
 
+const showUp = css.keyframes('showup', {
+  '0%': {position: 'relative', opacity:'0', transform: 'rotate(2880deg)'},
+  '70%': {position: 'relative', opacity:'1', transform: 'rotate(360deg)' },
+  '90%': {position: 'relative', opacity:'1', transform: 'rotate(0deg)' },
+  '100%': {position: 'relative', opacity:'1', transform: 'rotate(0deg)'},
+})
+
+const slideIn = css.keyframes('slidein', {
+  '0%': { marginLeft:'-400px' },
+  '20%': { marginLeft:'-400px' },
+  '90%': { marginLeft:'0' },
+  '100%': { marginLeft:'0' },
+})
+
 const portfolioLinkStyle = css({
   textDecoration: 'none',
   height: '100%',
@@ -29,27 +43,38 @@ const flexChild = css({
   background: '#222',
   height: 175,
   flex: '1 0 30%',
+  minWidth: 280,
   textAlign: 'left',
   border: '1px solid #e5e5e5',
   '&:hover': {
-    boxShadow: `0 2px 4px #4b4b4b`,
+    boxShadow: `0 2px 4px #C2ABCE`,
     cursor: 'pointer',
   },
 });
 
 const flexChildText = css({
-  color: '#96879E',
+  color: '#725182',
   fontSize: '1rem',
   width: '100%',
   height: '100%',
   textDecoration: 'none',
   paddingLeft: '0.5rem',
   margin: '0 auto',
+  overflow: 'hidden',
   '&:hover': {
     textDecoration: 'none',
-    color: '#632A7F',
+    color: '#96879E',
   },
 });
+
+const flexChildTextFirstLetter = css({
+  display: 'inline-block',
+  animation: `${showUp} 7s 1 ease-in-out`,
+})
+
+const flexChildTextRest = css({
+  animation: `${slideIn} 7s 1 ease-out`,
+})
 
 const bgMaker = (img) => (img ? `url("${img}") no-repeat` : `content-box radial-gradient(#d5d5d5, #fff)`);
 
@@ -70,7 +95,10 @@ export default function PortfolioIndex({ data }) {
               }}>
                 <Link className={portfolioLinkStyle}
                 to={post.frontmatter.path}>
-                  <h3 className={flexChildText}>{post.frontmatter.title}</h3>
+                  <h3 className={flexChildText}>
+                    <span className={flexChildTextFirstLetter}>{post.frontmatter.title.charAt(0)}</span>
+                    <span className={flexChildTextRest}>{post.frontmatter.title.substr(1)}</span>
+                  </h3>
                 </Link>
               </div>
             ))}
