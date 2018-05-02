3dd022dc29dcf2bdc088a225adb246e441f3e46c
Russell Schmidt <russell.schmidt@me.com>
2018-05-02 09:52:31 -0700

    get rid of box focus around buttons

--- src/components/AnimationPage.js
+++ src/components/AnimationPage.js
@@ -3,5 +3,6 @@ import React from 'react'
 export default () => (
   <div className="animation-page">
     <h1 className="animation-page__title">Hello</h1>
+    <button className="animation-page__button">Do Something</button>
   </div>
 );
