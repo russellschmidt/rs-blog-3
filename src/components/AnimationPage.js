55a00785c470bc8acc314b8ab7801754d8b56558
Russell Schmidt <russell.schmidt@me.com>
2018-05-06 23:15:26 -0700

    add animation to home page

--- src/components/AnimationPage.js
+++ src/components/AnimationPage.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default () => (
   <div className="animation-page">
-    <h1 className="animation-page__title">Hello</h1>
-    <button className="animation-page__button">Do Something</button>
+    <canvas id='canvas' width='300' height='300'>
+      Animations are fun. Though, if you see this, your browser does not support the 'canvas' tag so you are not having maximum fun.
+    </canvas>
   </div>
 );
