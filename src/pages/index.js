637634ffda1a8bd4900e87640f82ef8af13d7174
Russell Schmidt <russell.schmidt@me.com>
2018-05-09 19:20:21 -0700

    removed game

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,9 @@ import { css } from "glamor";
 
 import {rhythm} from "../utils/typography";
 
-import GameStart from "../components/GameStart";
-
 export default () => (
   <div>
     <Helmet title={`Welcome to Russell Schmidt's home on the internet`}/>
     <h1>Welcome</h1>
-    <GameStart />
   </div>
 );
